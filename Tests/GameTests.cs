d4118ccab0e9e13182e5aab0b82b88f06f1f7360
NikosGour <63046380+NikosGour@users.noreply.github.com>
2026-04-06 12:52:43 +0300

    add more test cases for score point

--- Tests/GameTests.cs
+++ Tests/GameTests.cs
@@ -11,6 +11,11 @@ public class GameTests
     [InlineData(Points.THIRTY, Points.LOVE, Points.FORTY, Teams.HOME)]
     [InlineData(Points.FORTY, Points.LOVE, Points.LOVE, Teams.HOME)]
     [InlineData(Points.LOVE, Points.LOVE, Points.FIFTEEN, Teams.AWAY)]
+    [InlineData(Points.LOVE, Points.FIFTEEN, Points.THIRTY, Teams.AWAY)]
+    [InlineData(Points.LOVE, Points.THIRTY, Points.FORTY, Teams.AWAY)]
+    [InlineData(Points.LOVE, Points.FORTY, Points.LOVE, Teams.AWAY)]
+    [InlineData(Points.FORTY, Points.FORTY, Points.ADVANTAGE, Teams.HOME)]
+    [InlineData(Points.FORTY, Points.FORTY, Points.ADVANTAGE, Teams.AWAY)]
     public void TestScorePoint(Points homePlayerPoints, Points awayPlayerPoints, Points expectedPoints, Teams pointScoringTeam)
     {
         Game game = new(homePlayerPoints, awayPlayerPoints);
