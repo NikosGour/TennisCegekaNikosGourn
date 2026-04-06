e4e9d6659131d283a7d79bbb7f18c01e6ada9340
NikosGour <63046380+NikosGour@users.noreply.github.com>
2026-04-06 14:25:56 +0300

    add fields and constructors

--- Library/Game.cs
+++ Library/Game.cs
@@ -2,21 +2,27 @@
 
 internal class Game
 {
-    int homeTeamSets;
-    int awayTeamSets;
+    private int homeTeamSets = 0;
+    private int awayTeamSets = 0;
 
-    int homeTeamPoints;
-    int awayTeamPoints;
+    private Points homeTeamPoints = Points.LOVE;
+    private Points awayTeamPoints = Points.LOVE;
 
     public Game(Points homeTeamPoints, Points awayTeamPoints) : this()
     {
+        this.homeTeamPoints = homeTeamPoints;
+        this.awayTeamPoints = awayTeamPoints;
     }
     public Game(int homeTeamSets, int awayTeamSets) : this(Points.LOVE, Points.LOVE)
     {
+        this.homeTeamSets = homeTeamSets;
+        this.awayTeamSets = awayTeamSets;
     }
 
     public Game(Points homeTeamPoints, Points awayTeamPoints, int homeTeamSets, int awayTeamSets) : this(homeTeamPoints, awayTeamPoints)
     {
+        this.homeTeamSets = homeTeamSets;
+        this.awayTeamSets = awayTeamSets;
     }
 
     public Game()
