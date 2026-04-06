e8d0d7f19d9694f2facf66d7ac7d673a11b42a50
NikosGour <63046380+NikosGour@users.noreply.github.com>
2026-04-06 15:54:13 +0300

    add dummy main

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using TennisCegekaNikosGourn.Library;
+
+namespace TennisCegekaNikosGourn;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Game game = new(Points.FORTY, Points.THIRTY, 3, 2);
+        game.TeamScores(Teams.HOME);
+        if (game.hasWonGame(Teams.HOME))
+        {
+            Console.WriteLine("Home team wins the game!");
+        }
+        else if (game.hasWonGame(Teams.AWAY))
+        {
+            Console.WriteLine("Away team wins the game!");
+        }
+        else
+        {
+            Console.WriteLine("The game is still ongoing.");
+        }
+    }
+}
